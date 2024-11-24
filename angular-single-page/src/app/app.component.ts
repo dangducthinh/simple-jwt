aa53ddf26e6b5fd4544e4915ad7b103ef7475e2c
Thinh Dang <thinh.dang@groovetechnology.com>
2024-11-24 17:39:38 +0700

    Some logic for check accessToken

--- angular-single-page/src/app/app.component.ts
+++ angular-single-page/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './login/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(private authService: AuthService, private router: Router){}
   title = 'angular-single-page';
+
+  isAuthenticated () {
+    return this.authService.isAuthenticated();
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login'])
+  }
 }
